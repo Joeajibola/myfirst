6e4596e1c6bf54ed1bb65c32d89b8b3e5c3509fe
Joeajibola <ajibolajoseph9@gmail.com>
2021-01-12 11:20:29 -0800

    Adding my practice work

---
+++ practice.py
@@ -0,0 +1,7 @@
+print("Practice Exercise 2: ")
+message = ("Hope you're having fun!")
+print("The message is", message)
+
+# perform mathematical operations
+answer = 5 + 2 ** 3
+print("The answer is", answer)
